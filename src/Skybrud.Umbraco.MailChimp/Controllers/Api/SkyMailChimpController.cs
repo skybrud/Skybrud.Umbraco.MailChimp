691b3e5e338f4b7d2573d80cd8cf3e880df67659
René Pjengaard Bank <rpjengaard@skybrud.dk>
2017-07-06 16:10:26 +0200

    [Skybrud] Security update

--- src/Skybrud.Umbraco.MailChimp/Controllers/Api/SkyMailChimpController.cs
+++ src/Skybrud.Umbraco.MailChimp/Controllers/Api/SkyMailChimpController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using MailChimp.Lists;
 using Skybrud.Umbraco.MailChimp.Integrations;
 using Skybrud.Umbraco.MailChimp.Models;
 using Skybrud.WebApi.Json;
+using umbraco.cms.presentation;
 using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.WebApi;
 
 namespace Skybrud.Umbraco.MailChimp.Controllers.Api
@@ -64,9 +69,10 @@ namespace Skybrud.Umbraco.MailChimp.Controllers.Api
         /// <param name="email"></param>
         /// <param name="contextid"></param>
         /// <param name="listid"></param>
+        /// <param name="subscriberId"></param>
         /// <returns>SkyMailChimpSubscriber</returns>
         [HttpGet]
-        public object GetSubscriber(string email, string contextid, string listid)
+        public object GetSubscriber(string email, string contextid, string listid, string subscriberId = "")
         {
             HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
 
@@ -90,10 +96,19 @@ namespace Skybrud.Umbraco.MailChimp.Controllers.Api
 
                 if (subscriber != null)
                 {
-                    var mcs = new SkyMailChimpSubscriber(subscriber);
+                    if (subscriber.Id == subscriberId)
+                    {
+                        // all okay, load the user
+                        var mcs = new SkyMailChimpSubscriber(subscriber);
 
-                    // return SkyMailChimpSubscriber object
-                    return Request.CreateResponse(mcs);
+                        // return SkyMailChimpSubscriber object
+                        return Request.CreateResponse(mcs);
+                    }
+                    else
+                    {
+                        // subscriberId missing, throw error
+                        return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                    }
                 }
 
                 // no subscriber found, send empty SkyMailChimpSubscriber
@@ -102,11 +117,79 @@ namespace Skybrud.Umbraco.MailChimp.Controllers.Api
             }
             catch (Exception ex)
             {
-                LogHelper.Error<SkyMailChimpController>("mailchimp error", ex);
+                LogHelper.Error<SkyMailChimpController>("mailchimp error (GetSubscriber)", ex);
 
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
             }
         }
+
+        /// <summary>
+        /// Send updatelink to user
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="contextid"></param>
+        /// <param name="listid"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object SendUpdateLink(string email, string contextid, string listid)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            try
+            {
+                SkyMailChimpHelper helper = new SkyMailChimpHelper(SkyMailChimpRepository.GetApiKey(contextid), listid, email);
+                MemberInfo subscriber = helper.GetSubscriber();
+                IPublishedContent currentPage = UmbracoContext.ContentCache.GetById(int.Parse(contextid));
+                string host = HttpContext.Current.Request.Url.Host;
+                var scheme = HttpContext.Current.Request.Url.Scheme;
+
+                if (currentPage == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "contenxt not existing");
+                }
+
+                // build update url
+                string updateUrl = string.Format("{0}://{1}{2}?email={3}&id={4}", scheme, host, currentPage.Url, subscriber.Email, subscriber.Id);
+
+                // fetch settings from Umbraco
+                string subject = currentPage.GetPropertyValue<string>("skyMailChimpUpdateMailSubject", true);
+                string body = currentPage.GetPropertyValue<string>("skyMailChimpUpdateMailBody", true);
+
+                // set default values if non found
+                subject = string.IsNullOrWhiteSpace(subject) ? "Rediger din nyhedsbrevsprofil" : subject;
+                body = string.IsNullOrWhiteSpace(body) ? "<a href=\"{updateLink}\">Opdater din profil</a>" : body;
+
+                // replace body tags
+                body = body.Replace("{updateLink}", updateUrl);
+
+                // send mail
+                MailMessage msg = new MailMessage();
+                msg.To.Add(subscriber.Email);
+                msg.Subject = subject;
+                msg.BodyEncoding = Encoding.UTF8;
+                msg.IsBodyHtml = true;
+
+                msg.Body = body;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Send(msg);
+                }
+
+                LogHelper.Info<SkyMailChimpController>(string.Format("Mail send to: {0} ({1})", subscriber.Id, subscriber.Email));
+
+                return Request.CreateResponse(true);
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<SkyMailChimpController>("mailchimp error (SendUpdateLink)", ex);
+
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
         #endregion
+
     }
 }
